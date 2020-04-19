f14457f637e51fb1f1c14b1d70ab86236a2bd0ee
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2020-04-19 13:03:38 +0200

    add testing api controller

---
+++ spec/api/v1/movies_seasons_controller_spec.rb
@@ -0,0 +1,51 @@
+RSpec.describe Api::V1::MoviesSeasonsController, type: :controller do
+
+    describe 'GET all movies and season' do
+        before do
+            api_access
+            @user = create :user
+            page.driver.header 'USER_AUTH_TOKEN', @user.auth_token
+            30.times do
+                create(:movie)
+            end 
+            20.times do
+                create(:season)
+            end  
+        end
+
+        it 'should have all movies and season data...' do
+            page.driver.get(api_v1_movies_seasons_path)
+            expect(page.status_code).to be(200)  
+            
+            data = JSON.parse(page.body)
+
+            expect(data["movies"].first.has_key? "title").to be true
+            expect(data["movies"].first.has_key? "plot").to be true
+            expect(data["movies"].count).to match Api::V1::ApiController::ITEMS_PER_PAGE_API
+  
+            expect(data["seasons"].first.has_key? "title").to be true
+            expect(data["seasons"].first.has_key? "plot").to be true
+            expect(data["seasons"].count).to match Api::V1::ApiController::ITEMS_PER_PAGE_API
+            expect(data["meta"]["current_page"]).to match 1
+            expect(data["movies"].count).to be >= 0
+            expect(data["seasons"].count).to be >= 0
+                       
+        end
+
+        it 'all movies and seasons ...' do
+            page.driver.get(api_v1_movies_seasons_path)
+            expect(page.status_code).to be(200)  
+            
+            data = JSON.parse(page.body)
+            
+            expect(data["meta"]["total_count"]).to match (Season.count + Movie.count)
+        end
+
+
+        it 'should have all movies and seasons order by created_at ...' do
+            expect(Movie.order_by_create.to_sql).to eq Movie.order(created_at: :asc).to_sql
+            expect(Season.order_by_create.to_sql).to eq Season.order(created_at: :asc).to_sql
+        end
+    end
+
+end
