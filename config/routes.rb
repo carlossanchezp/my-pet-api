3653ab48cadb6c48b6a4e93fb6f3e5f99354bc13
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2020-04-19 12:33:08 +0200

    refactor test routing season and routing only index show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :episodes
   namespace :api do
     namespace :v1 do
-      resources :seasons
+      resources :seasons, only: [:index, :show]
 
       resources :movies, only: [:index, :show]
     end
--- spec/routing/seasons_routing_spec.rb
+++ spec/routing/seasons_routing_spec.rb
@@ -7,5 +7,24 @@ RSpec.describe Api::V1::SeasonsController, type: :routing do
       expect(get: "api/v1/seasons").to route_to(:controller => "api/v1/seasons" , :action => "index")
     end
 
+    it "routes to #show" do
+      expect(get: "api/v1/seasons/1").to be_routable
+    end
+
+    it "routes to #create" do
+      expect(post: "api/v1/seasons").not_to be_routable
+    end
+
+    it "routes to #update via PUT" do
+      expect(put: "api/v1/seasons/1").not_to be_routable
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "api/v1/seasons/1").not_to be_routable
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "api/v1/seasons/1").not_to be_routable
+    end
   end
 end
