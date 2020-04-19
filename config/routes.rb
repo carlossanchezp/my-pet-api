dd4fd704b52c304f4fd505683402a5305ad53f21
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2020-04-19 13:05:40 +0200

    add movies and season endpoint all together and controller testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'movies_seasons/index'
+  resources :episodes
   namespace :api do
     namespace :v1 do
       resources :seasons, only: [:index, :show]
-
       resources :movies, only: [:index, :show]
+      get '/movies_seasons', to: 'movies_seasons#index'
     end
   end
 
